3ad0fc9b76210f8bd9fe320107df775f210e6250
hyunsooSong <84562885+myhuon@users.noreply.github.com>
2021-07-18 15:46:19 +0900

    test

--- app/src/main/java/com/example/care/MainActivity.java
+++ app/src/main/java/com/example/care/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //hihihiihhi
         setContentView(R.layout.activity_main);
 
         Button go = findViewById(R.id.go);
