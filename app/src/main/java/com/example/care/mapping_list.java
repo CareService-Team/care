2e641532db59104f86237595b6f83f65c83dbdd3
yaj2n <54498996+yaj2n@users.noreply.github.com>
2021-07-26 17:01:39 +0900

    7/26

---
+++ app/src/main/java/com/example/care/mapping_list.java
@@ -0,0 +1,13 @@
+package ddwucom.mobile.finalproject.careservice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mapping_list extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapping_list);
+    }
+}
---
+++ app/src/main/java/com/example/care/my_info.java
@@ -0,0 +1,20 @@
+package ddwucom.mobile.finalproject.careservice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class my_info extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_info);
+
+        Button button = findViewById(R.id.button);
+
+
+    }
+}
