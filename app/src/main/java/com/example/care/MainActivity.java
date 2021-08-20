5b6fcc1a1fb9c9daa92e57cf99d32427b08e5c35
hyunsooSong <84562885+myhuon@users.noreply.github.com>
2021-08-21 08:29:24 +0900

    facebook login

--- app/src/main/java/com/example/care/MainActivity.java
+++ app/src/main/java/com/example/care/MainActivity.java
@@ -3,10 +3,41 @@ package com.example.care;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+//import com.nhn.android.naverlogin.OAuthLogin;
+//import com.nhn.android.naverlogin.OAuthLoginHandler;
+
 public class MainActivity extends AppCompatActivity {
+    //Client 정보
+    private static String OAUTH_CLIENT_ID = "mUsAjhZRKQ6B6EYt9aSf";
+    private static String OAUTH_CLIENT_SECRET = "1KexLwm3_7";
+    private static String OAUTH_CLIENT_NAME = "care";
+
+    private static OAuthLogin mOAuthLoginInstance;
+    private static OAuthLoginButton mOAuthLoginButton;
+    private static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +60,76 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mContext = this;
+
+        //초기화
+        initData();
+
+        //해시키
+        getHashKey();
+    }
+
+    private void initData() {
+        //초기화
+        mOAuthLoginInstance = OAuthLogin.getInstance();
+        mOAuthLoginInstance.init(mContext, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_CLIENT_NAME);
+        mOAuthLoginButton = (OAuthLoginButton) findViewById(R.id.btnNaverLogin);
+        mOAuthLoginButton.setOAuthLoginHandler(mOAuthLoginHandler);
+    }
+
+    /*
+    OAuthLoginHandler를 startOAuthLoginActivity() 메서드 호출 시 파라미터로 전달하거나
+    OAuthLoginButton 객체에 등록하면 인증 종료 확인 가능
+    */
+
+    private OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+        @Override
+        public void run(boolean success) {
+            if (success){
+                String accessToken = mOAuthLoginInstance.getAccessToken(mContext);
+                String refreshToken = mOAuthLoginInstance.getRefreshToken(mContext);
+                long expiresAt = mOAuthLoginInstance.getExpiresAt(mContext);
+                String tokenType = mOAuthLoginInstance.getTokenType(mContext);
+
+                Toast.makeText(mContext, "success:" + accessToken, Toast.LENGTH_SHORT).show();
+
+                //이동 할 액티비티
+                redirectSignupActivity();
+
+            }else{
+                String errorCode = mOAuthLoginInstance.getLastErrorCode(mContext).getCode();
+                String errorDesc = mOAuthLoginInstance.getLastErrorDesc(mContext);
+                Toast.makeText(mContext, "errorCode:" + errorCode + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    private void redirectSignupActivity() {
+        final Intent intent = new Intent(this, MyPage.class);
+        startActivity(intent);
+        finish();
+    }
+
+    //해시키 구하기
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
     }
 }
